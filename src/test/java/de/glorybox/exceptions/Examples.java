a59e6c12de4386ea5bba0f8f4d257218c5ea2f07
Kai Weber <kai.weber@brands4friends.de>
2019-12-09 07:52:59 +0100

    Update

--- src/test/java/de/glorybox/exceptions/Examples.java
+++ src/test/java/de/glorybox/exceptions/Examples.java
@@ -1,7 +1,7 @@
 package de.glorybox.exceptions;
 
 public class Examples {
-	
+
     public static String duplicatesShortStrings(String input) throws ExceptionsTest.InputTooLongException {
         if (input.length() > 1) throw new ExceptionsTest.InputTooLongException();
         return input + input;
--- src/test/java/de/glorybox/exceptions/ExceptionsTest.java
+++ src/test/java/de/glorybox/exceptions/ExceptionsTest.java
@@ -6,70 +6,90 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
+import io.vavr.CheckedFunction1;
+import io.vavr.control.Try;
+
 public class ExceptionsTest {
 
-	@Test
-	void foreach() {
+    @Test
+    void foreach() {
 
-		var input = List.of("a", "b", "c", "toolong", "e", "f");
-		var result = new ArrayList<String>();
-		var handler = new ErrorHandler();
-		
-		try {
-			for (String string : input) {
-				result.add(Examples.duplicatesShortStrings(string));
-			}
-		} catch (InputTooLongException e) {
-			handler.accept(e);
-		}
+        var input = List.of("a", "b", "c", "toolong", "e", "f");
+        var result = new ArrayList<String>();
+        var handler = new ErrorHandler();
 
-		assertEquals(List.of("aa", "bb", "cc"), result);
-		assertEquals(1, handler.called);
-	}
-	
-	@Test
-	void stream() {
-		var input = List.of("a", "b", "c", "toolong", "e", "f");
-		var handler = new ErrorHandler();
+        try {
+            for (String string : input) {
+                result.add(Examples.duplicatesShortStrings(string));
+            }
+        } catch (InputTooLongException e) {
+            handler.accept(e);
+        }
 
-		List<String> result = new ArrayList<>();
-		
-		try {
-			result = input
-					.stream()
-					.map(arg0 -> {
-						try {
-							return Examples.duplicatesShortStrings(arg0);
-						} catch (InputTooLongException e) {
-							throw new RuntimeException(e);
-						}
-					}).collect(Collectors.toList());
-				
-		} catch (Exception e) {
-			handler.accept(e);
-		}
-		
-		
-		assertEquals(List.of("aa", "bb", "cc"), result);
-		assertEquals(1, handler.called);
+        assertEquals(List.of("aa", "bb", "cc"), result);
+        assertEquals(1, handler.called);
+    }
 
-	}
-	
+    @Test
+    void stream() {
+        var input = Stream.of("a", "b", "c", "toolong", "e", "f");
+        var handler = new ErrorHandler();
+        final var result = new ArrayList<>();
 
-	public static class InputTooLongException extends Exception {
-		private static final long serialVersionUID = 1L;
-	}
-	
-	public static class ErrorHandler implements Consumer<Throwable>{
-		int called = 0; 
+        try {
+            input.map(item -> {
+                try {
+                    return Examples.duplicatesShortStrings(item);
+                } catch (InputTooLongException e) {
+                    throw new RuntimeException(e);
+                }
+            }).map(item -> {
+                result.add(item);
+                return item;
+            }).forEach(a -> {});
 
-		@Override
-		public void accept(Throwable arg0) {
-			called++;
-		}
-	}
+        } catch (Exception e) {
+            handler.accept(e);
+        }
+
+        assertEquals(List.of("aa", "bb", "cc"), result);
+        assertEquals(1, handler.called);
+
+    }
+
+    @Test
+    void stream2() {
+        var input = Stream.of("a", "b", "c", "toolong", "e", "f");
+        var handler = new ErrorHandler();
+
+        var result =
+                input
+                    .map(CheckedFunction1.liftTry(Examples::duplicatesShortStrings))
+                    .map(t -> t.onFailure(handler))
+                    //.takeWhile(Try::isSuccess)
+                    .flatMap(Try::toJavaStream)
+                    .collect(Collectors.toList());
+
+        assertEquals(List.of("aa", "bb", "cc"), result);
+        assertEquals(1, handler.called);
+
+    }
+
+    public static class InputTooLongException extends Exception {
+        private static final long serialVersionUID = 1L;
+    }
+
+    public static class ErrorHandler implements Consumer<Throwable> {
+        int called = 0;
+
+        @Override
+        public void accept(Throwable arg0) {
+            called++;
+        }
+    }
 
 }
